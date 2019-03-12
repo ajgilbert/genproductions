6da43ee7b9ea0d12201aa3675a132340f663d749
Andrew Gilbert <andrew.gilbert@cern.ch>
2019-03-12 16:53:13 +0100

    adding changes for EFT gridpacks

--- bin/MadGraph5_aMCatNLO/gridpack_generation.sh
+++ bin/MadGraph5_aMCatNLO/gridpack_generation.sh
@@ -193,8 +193,8 @@ make_gridpack () {
           #get needed BSM model
           if [[ $model = *[!\ ]* ]]; then
             echo "Loading extra model $model"
-            wget --no-check-certificate https://cms-project-generators.web.cern.ch/cms-project-generators/$model	
-            # cp /afs/cern.ch/user/a/agilbert/public/$model ./
+            # wget --no-check-certificate https://cms-project-generators.web.cern.ch/cms-project-generators/$model	
+            cp /afs/cern.ch/user/a/agilbert/public/$model ./
             cd models
             if [[ $model == *".zip"* ]]; then
               unzip ../$model
